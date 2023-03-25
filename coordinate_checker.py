49308b0157798be91beabfee800684c9cc9de204
Hari25483 <haricodmw@gmail.com>
2023-03-25 13:05:51 +0530

    Draw line between those coordinates

--- coordinate_checker.py
+++ coordinate_checker.py
@@ -4,10 +4,6 @@ import numpy as np
 # Load the image
 image = cv2.imread("image.jpg")
 
-# Define the radius and thickness of the circle
-radius = 5
-thickness = 2
-
 # Create a window to display the image
 cv2.namedWindow("image")
 
@@ -18,25 +14,30 @@ def mouse_callback(event, x, y, flags, param):
         if len(points) < 2:
             points.append((x, y))
 
-    # Draw a circle around the clicked point
-    if len(points) == 1:
-        cv2.circle(image, points[0], radius, (0, 0, 255), thickness)
-
-    if len(points) == 2:
-        cv2.circle(image, points[1], radius, (0, 0, 255), thickness)
-        # Calculate the distance between the two points using Pythagoras' theorem
-        distance = np.sqrt((points[1][0] - points[0][0])**2 + (points[1][1] - points[0][1])**2)
-        # Print the distance in pixels
-        print("Distance in pixels:", distance)
-
 # Register the mouse callback function
 cv2.setMouseCallback("image", mouse_callback)
 
 # Display the image and wait for the user to select two points
 points = []
 while len(points) < 2:
+    # Draw a red circle around the selected points
+    for point in points:
+        cv2.circle(image, point, 5, (0, 0, 255), -1)
     cv2.imshow("image", image)
     cv2.waitKey(1)
+
+# Draw a red line between the two selected points
+cv2.line(image, points[0], points[1], (0, 0, 255), 2)
+
+# Calculate the distance between the two points using Pythagoras' theorem
+distance = np.sqrt((points[1][0] - points[0][0])**2 + (points[1][1] - points[0][1])**2)
+
+# Print the distance in pixels
+print("Distance in pixels:", distance)
+
+# Display the image with the selected points and line
+cv2.imshow("image", image)
+cv2.waitKey(0)
 
 # Close the window
 cv2.destroyAllWindows()
