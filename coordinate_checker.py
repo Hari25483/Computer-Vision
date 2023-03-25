76d84117a22b6dea941dad24a7efcd8e5133f8dd
Hari25483 <haricodmw@gmail.com>
2023-03-25 13:03:18 +0530

    Update coordinate_checker.py

--- coordinate_checker.py
+++ coordinate_checker.py
@@ -2,7 +2,11 @@ import cv2
 import numpy as np
 
 # Load the image
-image = cv2.imread("b.jpg")
+image = cv2.imread("image.jpg")
+
+# Define the radius and thickness of the circle
+radius = 5
+thickness = 2
 
 # Create a window to display the image
 cv2.namedWindow("image")
@@ -14,6 +18,17 @@ def mouse_callback(event, x, y, flags, param):
         if len(points) < 2:
             points.append((x, y))
 
+    # Draw a circle around the clicked point
+    if len(points) == 1:
+        cv2.circle(image, points[0], radius, (0, 0, 255), thickness)
+
+    if len(points) == 2:
+        cv2.circle(image, points[1], radius, (0, 0, 255), thickness)
+        # Calculate the distance between the two points using Pythagoras' theorem
+        distance = np.sqrt((points[1][0] - points[0][0])**2 + (points[1][1] - points[0][1])**2)
+        # Print the distance in pixels
+        print("Distance in pixels:", distance)
+
 # Register the mouse callback function
 cv2.setMouseCallback("image", mouse_callback)
 
@@ -22,12 +37,6 @@ points = []
 while len(points) < 2:
     cv2.imshow("image", image)
     cv2.waitKey(1)
-
-# Calculate the distance between the two points using Pythagoras' theorem
-distance = np.sqrt((points[1][0] - points[0][0])**2 + (points[1][1] - points[0][1])**2)
-
-# Print the distance in pixels
-print("Distance in pixels:", distance)
 
 # Close the window
 cv2.destroyAllWindows()
